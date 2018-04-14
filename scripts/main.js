28cf8c9bdc96a05b752bced218518c047424ba16
bc-whisnant <bc.whisnant@gmail.com>
2018-04-14 15:37:47 -0400

    added functionality for normal and hero buttons

--- scripts/main.js
+++ scripts/main.js
@@ -9,11 +9,8 @@ request.onload = function() {
     var data = JSON.parse(this.response);
     console.log(data);
 
-
-
     data.characters.forEach(character => {
         console.log(character.name);
-    
 
         const characterContainer = document.querySelector('.character-container');
 
@@ -35,9 +32,82 @@ request.onload = function() {
 
         characterContainer.appendChild(individualCharacter);
         individualCharacter.appendChild(characterImage);
-        individualCharacter.appendChild(characterName);        
+        individualCharacter.appendChild(characterName); 
     })
-
 }
 
 request.send();
+
+const buttonContainer = document.querySelector('.button-container');
+
+const normalButton = document.createElement('button');
+normalButton.setAttribute('class', 'normal');
+normalButton.innerHTML = 'Normal';
+
+const heroButton = document.createElement('button');
+heroButton.setAttribute('class', 'hero');
+heroButton.innerHTML = 'Hero';
+
+buttonContainer.appendChild(normalButton);
+buttonContainer.appendChild(heroButton);
+
+document.querySelector('.normal').addEventListener('click', function() {
+    console.log('clicked');
+})
+
+document.querySelector('.normal').addEventListener('click', function() {
+    var nodes = document.querySelectorAll('.character');
+    var first = nodes[0];
+    var firstImage = document.getElementsByTagName('img')[0];
+    firstImage.setAttribute('src', 'images/cartman.png');
+
+    var nodes = document.querySelectorAll('.character');
+    var second = nodes[1];
+    var secondImage = document.getElementsByTagName('img')[1];
+    secondImage.setAttribute('src', 'images/kenny.png');
+
+    var nodes = document.querySelectorAll('.character');
+    var third = nodes[2];
+    var thirdImage = document.getElementsByTagName('img')[2];
+    thirdImage.setAttribute('src', 'images/kyle.png');
+
+    var nodes = document.querySelectorAll('.character');
+    var fourth = nodes[3];
+    var fourthImage = document.getElementsByTagName('img')[3];
+    fourthImage.setAttribute('src', 'images/stan.png');
+    
+});
+
+document.querySelector('.hero').addEventListener('click', function() {
+    
+    var nodes = document.querySelectorAll('.character');
+    var nodeNames = document.querySelectorAll('.name');
+
+    var first = nodes[0];
+    var firstImage = document.getElementsByTagName('img')[0];
+    firstImage.setAttribute('src', 'images/cartman2.png');
+    var firstName = document.getElementsByTagName('h3')[0];
+    firstName.innerText = 'The Coon';
+    // console.log(first);
+    var nodes = document.querySelectorAll('.character');
+    var second = nodes[1];
+    var secondImage = document.getElementsByTagName('img')[1];
+    secondImage.setAttribute('src', 'images/kenny2.png');
+    var second = document.getElementsByTagName('h3')[1];
+    second.innerText = 'Mysterion';
+
+    var nodes = document.querySelectorAll('.character');
+    var third = nodes[2];
+    var thirdImage = document.getElementsByTagName('img')[2];
+    thirdImage.setAttribute('src', 'images/kyle2.png');
+    var third = document.getElementsByTagName('h3')[2];
+    third.innerText = 'The Human Kite';
+
+    var nodes = document.querySelectorAll('.character');
+    var fourth = nodes[3];
+    var fourthImage = document.getElementsByTagName('img')[3];
+    fourthImage.setAttribute('src', 'images/stan2.png');
+    var fourth = document.getElementsByTagName('h3')[3];
+    fourth.innerText = 'Toolshed';
+    
+});
